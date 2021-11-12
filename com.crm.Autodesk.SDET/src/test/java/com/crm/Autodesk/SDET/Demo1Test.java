5355686e17cb33d2598bcb35d00c2f2d34ad035b
VHK <VHK@VINAY>
2021-11-12 12:01:57 +0530

    AutoDesk demo1

---
+++ com.crm.Autodesk.SDET/src/test/java/com/crm/Autodesk/SDET/Demo1Test.java
@@ -0,0 +1,12 @@
+package com.crm.Autodesk.SDET;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	@Test
+	public void demo1Test() {
+		System.out.println("demo 1 test");
+		
+	}
+
+}
