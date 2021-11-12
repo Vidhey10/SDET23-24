9b282f2c2053db996b3db8c59a40486579d71fb0
VHK <VHK@VINAY>
2021-11-12 15:53:47 +0530

    commit 15 55

--- Github.Practice/src/test/java/githubPractice/DemoTest.java
+++ Github.Practice/src/test/java/githubPractice/DemoTest.java
@@ -1,6 +1,5 @@
 package githubPractice;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DemoTest 
@@ -9,5 +8,6 @@ public class DemoTest
 	public void demoTest() {
 		System.out.println("GitHub 1");
 		System.out.println("Github 2");
+		System.out.println("Github 3");
 	}
 }
